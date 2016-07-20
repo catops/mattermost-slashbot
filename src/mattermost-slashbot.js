d082d15073d5d2b1eeb05db426e8a25a6e39383e
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-07-20 13:45:32 -0700

    force end unmatch responses

--- src/mattermost-slashbot.js
+++ src/mattermost-slashbot.js
@@ -3,7 +3,7 @@
      A Hubot script that parses slash commands from Mattermost, forwards them to the bot, then formats the bot response to be slash command friendly.
 
    Dependencies:
-     None 
+     None
 
    Author:
      Wyatt Pearsall
@@ -12,35 +12,38 @@
 var bodyParser = require('body-parser');
 var hubot = require('hubot');
 
-var responseStore = {};
-
-function resolveResponse(tag, text){
-  responseStore[tag].end(JSON.stringify({response_type: 'in_channel', text: text}));
-  delete responseStore[tag];
+function resolveResponse(commandObj, text){
+  clearTimeout(commandObj.timeout);
+  if(commandObj.res){
+    commandObj.res.end(JSON.stringify({response_type: 'in_channel', text: text}));
+  }
 }
 
 module.exports = function(robot){
   robot.router.use(bodyParser.urlencoded({extended: false}));
 
   robot.responseMiddleware(function(context, next, done){
-    if(context.response.message.slashCommand){
-      resolveResponse(context.response.message.slashCommand, context.strings.join('\n'));
+    var command = context.response.message.slashCommand;
+    if(command){
+      delete context.response.message.slashCommand
+      resolveResponse(command, context.strings.join('\n'));
       context.strings = [];
     }
     next(done);
   });
 
   robot.router.post('/hubot/command', function(req, res){
-    console.log('message from mattermost');
     if(!req.body.command) return res.status(422).end();
-    console.log(req.body);
     var user = new hubot.User(req.body.user_id, {room: req.body.channel_id, name:req.body.user_name});
     var msg = new hubot.TextMessage(user, robot.name + ' ' + req.body.text)
 
-    var tag = Math.ceil(Math.random() * 1e17);
-
-    msg.slashCommand = tag;
-    responseStore[tag] = res;
+    msg.slashCommand = {
+      res: res,
+      timeout: setTimeout(function(){
+        res.end();
+        msg.slashCommand = null;
+      }, 3000)
+    };
 
     robot.receive(msg);
   });
